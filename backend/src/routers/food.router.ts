449e35474fea3ec40debfbf612d14a68492dda09
ShadowBlack <daniel.miliczek@op.pl>
2024-02-08 01:54:26 +0100

    Use data from DB

--- backend/src/routers/food.router.ts
+++ backend/src/routers/food.router.ts
@@ -18,30 +18,64 @@ router.get("/seed", asyncHandler(
   }
 ));
 
-router.get("/", (req, res) => {
-  res.send(sample_foods);
-});
+router.get("/", asyncHandler(
+  async (req, res) => {
+    const foods = await FoodModel.find();
+    res.send(foods);
+  }
+));
 
-router.get("/search/:searchTerm", (req, res) => {
-  const searchTerm = req.params.searchTerm;
-  const foods = sample_foods.filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
-  res.send(foods);
-}); 
+router.get("/search/:searchTerm", asyncHandler(
+  async (req, res) => {
+    const searchRegex = new RegExp(req.params.searchTerm, 'i');
+    const foods = await FoodModel.find({ name: {$regex: searchRegex} });
+    res.send(foods);
+  }
+)); 
 
-router.get('/tags', (req, res) => {
-  res.send(sample_tags);
-});
+router.get('/tags', asyncHandler(
+  async (req, res) => {
+    const tags = await FoodModel.aggregate([
+      {
+        $unwind: '$tags'
+      },
+      {
+        $group: {
+          _id: '$tags',
+          count: {$sum: 1}
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          name: '$_id',
+          count: '$count'
+        }
+      }
+    ]).sort({ count: -1 });
 
-router.get('/tags/:tagName', (req, res) => {
-  const tagName = req.params.tagName;
-  const foods = sample_foods.filter(food => food.tags?.includes(tagName));
-  res.send(foods);
-});
+    const all = {
+      name: "All",
+      count: await FoodModel.countDocuments()
+    }
 
-router.get('/:foodId', (req, res) => {
-  const foodId = req.params.foodId;
-  const food =  sample_foods.find(food => food.id === foodId);
-  res.send(food);
-});
+    tags.unshift(all);
+    res.send(sample_tags);
+  }
+));
+
+router.get('/tags/:tagName', asyncHandler(
+  async (req, res) => {
+    const foods = await FoodModel.find({ tags: req.params.tagName });
+    res.send(foods);
+  }
+));
+
+router.get('/:foodId', asyncHandler(
+  async (req, res) => {
+    const food = await FoodModel.findById(req.params.foodId);
+    res.send(food);
+  }
+));
 
 export default router;
--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -19,16 +19,18 @@ router.get("/seed", asyncHandler(
   }
 ));
 
-router.post('/login', (req, res) => {
-  const {email, password} = req.body;
-  const user = sample_users.find(user => user.email === email && user.password === password);
-
-  if (user) {
-    res.send(generateTokenResponse(user));
-  } else {
-    res.status(400).send("User name or password is not valid!");
+router.post('/login', asyncHandler(
+  async (req, res) => {
+    const {email, password} = req.body;
+    const user = await UserModel.findOne({ email, password })
+  
+    if (user) {
+      res.send(generateTokenResponse(user));
+    } else {
+      res.status(400).send("User name or password is not valid!");
+    }
   }
-});
+));
 
 const generateTokenResponse = (user: any) => {
   const token = jwt.sign({
