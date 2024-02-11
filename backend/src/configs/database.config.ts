9fbea727335a235dd3c3adb1b8749f6c4ea98787
ShadowBlack <daniel.miliczek@op.pl>
2024-02-11 13:33:12 +0100

    Update database.config.ts

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,6 +1,8 @@
 import { connect, ConnectOptions } from 'mongoose';
 
 export const dbConnect = () => {
+  console.log('Try to connect');
+  
   connect(process.env.MONGO_URI!).then(
     () => console.log("Connect successfull"),
     (error) => console.log(error)
