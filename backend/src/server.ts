e4a067bc80baebcba4a12646054c43af1a26756b
ShadowBlack <daniel.miliczek@op.pl>
2024-02-11 13:50:49 +0100

    change origin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: ["http://localhost:4200"]
+  origin: ["https://food-shop-shadowblack.netlify.app"]
 }));
 
 app.use('/api/foods', foodRouter);
--- frontend/src/app/shared/constants/urls.constnts.ts
+++ frontend/src/app/shared/constants/urls.constnts.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://food-shop-service-7de2.onrender.com';
 
 export const FOODS_URL = BASE_URL + '/api/foods';
 export const FOODS_TAGS_URL = FOODS_URL + '/tags';
